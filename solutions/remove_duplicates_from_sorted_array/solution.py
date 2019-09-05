2e9e2b618399c338f2f44b04f55e95af2aa6b2c7
lisiurday <lisiurday@gmail.com>
2019-09-05 19:13:21 +0800

    feat: remove duplicates from sorted array

--- solutions/remove_duplicates_from_sorted_array/solution.py
+++ solutions/remove_duplicates_from_sorted_array/solution.py
@@ -1,3 +1,36 @@
+from typing import List
+
+
 class Solution:
+    def sort(self, nums: List[int]):
+        self.quick_sort(nums, 0, len(nums) - 1)
+
+    def quick_sort(self, nums: List[int], start: int, end: int):
+        if start >= end:
+            return
+
+        base = nums[start]
+        p = start
+        q = end
+        while p < q:
+            while p < q and nums[q] > base:
+                q -= 1
+            nums[p] = nums[q]
+
+            while p < q and nums[p] <= base:
+                p += 1
+            nums[q] = nums[p]
+        nums[p] = base
+        self.quick_sort(nums, start, p - 1)
+        self.quick_sort(nums, p + 1, end)
+
     def removeDuplicates(self, nums: List[int]) -> int:
-        
+        # self.sort(nums)
+        if not nums:
+            return 0
+        index = 0
+        for num in nums:
+            if num != nums[index]:
+                index += 1
+                nums[index] = num
+        return index + 1
--- tests/test_remove_duplicates_from_sorted_array.py
+++ tests/test_remove_duplicates_from_sorted_array.py
@@ -6,11 +6,23 @@ class TestRemoveDuplicates(unittest.TestCase):
 
     def test_1(self):
         u_input = {
+            'nums': [0, 0, 1, 1, 1, 2, 2, 3, 3, 4],
         }
-        u_output = ()
+        u_output = 5
 
         solution = Solution()
         u_result = solution.removeDuplicates(**u_input)
+        self.assertEqual(u_result, u_output)
+
+    def test_sort(self):
+        u_input = {
+            'nums': [2, 5, 6, 8, 2, 1, 9],
+        }
+        u_output = [1, 2, 2, 5, 6, 8, 9]
+
+        solution = Solution()
+        solution.sort(**u_input)
+        u_result = u_input['nums']
         self.assertEqual(u_result, u_output)
 
 
